f8aef26669bdd6702a5c723228df46885e9691b9
phanvanthong <48062699+phanvanthong@users.noreply.github.com>
2020-08-25 21:25:30 +0700

    thêm code

---
+++ WebChatSocket/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebChatSocket.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("InsertUserName");
+        }
+
+        [HttpPost]
+        public IActionResult Index(string username)
+        {
+            return View("Index", username);
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
